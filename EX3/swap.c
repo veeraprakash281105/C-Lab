7a45b72600e7db4c3c8790e0c5a9e20be1ecd312
veeraprakash281105 <veeraprakash2811@gmail.com>
2026-04-20 12:01:49 +0545

    Create swap.c

---
+++ EX3/swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,temp;
+   printf("enter two numbers a and b");
+   scanf("%d%d",&a,&b);
+   temp=a;
+   a=b;
+   b=temp;
+   printf("after swapping a%d and b%d",a,b);
+   return 0;
+}
