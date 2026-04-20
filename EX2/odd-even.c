dbf006e904529f19e9c17e2cae73341ff8a20f83
veeraprakash281105 <veeraprakash2811@gmail.com>
2026-04-20 11:49:01 +0545

    Create odd-even.c

---
+++ EX2/odd-even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("enter any no");
+   scanf("%d",&n);
+   if(n%2==0)
+      printf("even number",&n);
+   else
+      printf("odd number",&n);
+   return 0;
+}
