8692497050f7ec80aadfe99fb1ff237ccca03ded
veeraprakash281105 <veeraprakash2811@gmail.com>
2026-04-20 11:40:53 +0545

    Create calculations.c

---
+++ EX1/calculations.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   float sum,sub,mult,div,a,b;
+   printf("enter the two numbers a and b");
+   scanf("%d%d",&sum,&sub,&mult,&div);
+   sum=a+b;
+   sub=a-b;
+   mult=a*b;
+   div=a/b;
+   printf("sum %d/n",sum);
+   printf("sub %d/n",sub);
+   printf("mult %d/n",mult);
+   printf("div %d/n",div);
+   return 0;
+}
